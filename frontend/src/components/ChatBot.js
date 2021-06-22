a3afe9fb9b2a7907b3574b4da41b455d0f4ed044
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-06-23 01:42:55 +0530

    minor changes

--- frontend/src/components/ChatBot.js
+++ frontend/src/components/ChatBot.js
@@ -5,12 +5,11 @@ const ChatBot = () => {
     <div>
       <h1>Any Questions? Ask our virtual assistant!</h1>
       <iframe
-        src="https://webchat.botframework.com/embed/sampleBot-prefab?s=2A26Wkeplyc.dSXsGOwmOryOSdjtI5wgvzA_c2QY32OqgMWorTUAPK8"
+        title="FaqBot"
+        src="https://webchat.botframework.com/embed/CS-AzureBot?s=2A26Wkeplyc.zinVOMGd220MLufHjMFs87rPKIkMWKq7XYWi-hslvdc"
         style={{ minWidth: "400px", width: "100%", minHeight: "500px" }}
-        title="FAQ BOT"
       ></iframe>
     </div>
   );
 };
-
 export default ChatBot;
