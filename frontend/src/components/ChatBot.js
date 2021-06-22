a38f8fe94c4f089d38d6efcc8a4d87e12491b9d7
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-06-22 19:05:57 +0530

    minor changes

--- frontend/src/components/ChatBot.js
+++ frontend/src/components/ChatBot.js
@@ -3,6 +3,7 @@ import React from "react";
 const ChatBot = () => {
   return (
     <div>
+      <h1>FAQ</h1>
       <iframe
         src="https://webchat.botframework.com/embed/sampleBot-prefab?s=A4MgH1elwzs.MyiCW0DNkcOD4jPyldw6kMa3hacFNjVp_gRIdl7eihE"
         style={{ minWidth: "400px", width: "100%", minHeight: "500px" }}
