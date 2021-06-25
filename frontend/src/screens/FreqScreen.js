0ec4aa7a24ebdee4913510cbda0c8522290da2a5
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-06-25 11:20:31 +0530

    minor changes

--- frontend/src/screens/FreqScreen.js
+++ frontend/src/screens/FreqScreen.js
@@ -4,30 +4,30 @@ const FreqScreen = () => {
   return (
     <div>
       <h3>Frequently Asked Questions</h3>
-      <h5>
+      <h5 className="freq">
         <strong>What are Pods?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         A pod is a factory-made unit for utilities like Toilet, Office, Gym
         etc., which is fully fitted and plumbed, delivered to the construction
         site, and ready to use. The on-site crews simply connect the pods to the
         main mechanical, electrical and plumbing systems in the corridors and
         pods above/below.
-      </h6>
+      </p>
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>Can I Customise Pods?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Yes, you can! It depends on sellers if they can build custom pods
         (Custom size, features etc. ) otherwise if the interior decoration is
         concerned then every seller provides this service.
-      </h6>
+      </p>
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>What are prefab Homes?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Prefabricated homes, often referred to as prefab homes or simply
         prefabs, are manufactured off-site in advance, usually in standard
         sections that can be easily shipped and assembled. Below are the
@@ -40,19 +40,17 @@ const FreqScreen = () => {
         pulled by a vehicle. - Unconventional Home Units such as bamboo,
         ferrocement &amp; mud homes will also be included after proper
         verification in future.
-      </h6>
+      </p>
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>What Kind of Prefab Houses do we sell?</strong>
       </h5>
-      <h6>
-        A)
-        <br /> Ready to move Prefab property: Buyer can buy/rent verified Prefab
+      <p className="freq">
+        A) Ready to move Prefab property: Buyer can buy/rent verified Prefab
         properties that are available readily built in the desired location.
         <br />
         <br />
-        B) <br />
-        Customisable Prefab Homes: If the buyer has his own land and wants to
+        B) Customisable Prefab Homes: If the buyer has his own land and wants to
         build/buy a Prefab home then he’ll be able to connect with the verified
         sellers that are available in his area. He can buy default factory-built
         homes (minimal customisation) or buy customised on-demand prefab homes.
@@ -60,30 +58,30 @@ const FreqScreen = () => {
         <br />
         C) Mobile Homes: Can be moved around (portable) and set-up easily. (
         Customisation may not be available )
-      </h6>
+      </p>
 
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>How do I sell on PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Go to seller registration &gt; Fill the form &gt; Once verified by our
         team you’ll be able to list down your products.
-      </h6>
+      </p>
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>How do I create a listing in PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Once you’re registered <strong>as a seller</strong> , you should see an
         admin option on the navbar.
         <br /> Admin &gt; Products &gt; Create Products
-      </h6>
+      </p>
       <br />
-      <h5>
+      <h5 className="freq">
         <strong>What is seller membership in PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Since we have just launched, we have a single seller membership package
         :<br />
         % fee on every sale of Pods. ( depends on product )<br />
@@ -93,25 +91,25 @@ const FreqScreen = () => {
         Monthly fee of Rs. 500.
         <br />
         Free customer track and seller support to get the best deals.
-      </h6>
+      </p>
       <br />
 
-      <h5>
+      <h5 className="freq">
         <strong>Can I trust sellers on PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         Of course! We onboard the best sellers in your area with verified
         practices and certifications. Moreover, buying through our platform
         guarantees free Prefab Home / Pod servicing for a minimum of 6 months to
         1 Year ( Seller Dependent )
-      </h6>
+      </p>
 
       <br />
 
-      <h5>
+      <h5 className="freq">
         <strong>Why buy from PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         We’re the only platform that sells prefabricated structures in the most
         transparent &amp; convenient way. We onboard the best sellers that
         suffice our stringent seller verification requirements and deliver
@@ -119,34 +117,34 @@ const FreqScreen = () => {
         return on any problems that arise*. We strive to drive down the prices
         of prefab structures as we grow and become the pioneers of this
         industry, hence we’ll provide the best value in return as possible.
-      </h6>
+      </p>
 
       <br />
 
-      <h5>
+      <h5 className="freq">
         <strong>Why Sell on PrefabMart?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         We’re the only platform that sells the prefabricated structures in the
         most transparent &amp; convenient way while abiding by seller demands
         during the registration process to get the best deals for a set fee. We
         help in getting verified leads through our platform, facilitate a
         convenient buying process, resolve conflicts and promote sellers on
         social media which enables mutual growth.
-      </h6>
+      </p>
 
       <br />
 
-      <h5>
+      <h5 className="freq">
         <strong>How do I contact support?</strong>
       </h5>
-      <h6>
+      <p className="freq">
         For technical issues please contact
         <br /> Abhuday: abhuday.mishra@hotmail.com, 9818946005
         <br />
         <br /> For any other issues please contact
         <br /> Rohit: rohit142001singh@gmail.com, 9044999570
-      </h6>
+      </p>
     </div>
   );
 };
